223595a3a696f198005885c7b584fa57ac365481
Vlad Shestavetskyi <vladislavshsh@gmail.com>
2022-09-22 16:05:08 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,29 @@ import { nanoid } from 'nanoid';
  import { FilterContacts } from "./Filter/filter";
 class App extends Component {
      state = {
-       contacts: [ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+       contacts: [],
        filter: '',
       
+     }
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem("contacts"));
+
+    console.log("contacts", contacts);
+    if (contacts?.length) {
+      this.setState({
+        contacts,
+      })
+    }
+
+  }
+  componentDidUpdate(prevProps, prevState) {
+    
+    const { contacts } = this.state;
+
+    if (prevState.contacts !== contacts) {
+       localStorage.setItem("contacts", JSON.stringify(contacts))
+    }
+   
   }
   addContacts = (data) => {
     if (this.isDublicate(data)) {
